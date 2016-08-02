00a5c2670afe68005e486299370863d3d2777593
jaygr1 <jrib6@yahoo.com>
2016-08-02 09:56:26 -0400

    wip chp 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 end
